99a9580b1cda7cd4fb4aec791328003fefc83253
VaishnaviSunil <johndoe@example.com>
2022-11-09 00:22:07 +0530

    first

--- furniture/settings.py
+++ furniture/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'furniture_home/static')
 ]
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
