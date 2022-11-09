bf541f11ec743765259985dded9528f6113c307f
VaishnaviSunil <johndoe@example.com>
2022-11-09 13:27:34 +0530

    new

--- furniture/urls.py
+++ furniture/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('furniture_home/',include('furniture_home.urls')),
+    path('',include('furniture_home.urls')),
 ]
